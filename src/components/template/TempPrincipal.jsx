7224d9789ad57c646046cdcb46fa53926264b6cb
fernandedfa <deivisondfaa@gmail.com>
2022-06-30 21:37:49 -0300

    add React Route

--- src/components/template/TempPrincipal.jsx
+++ src/components/template/TempPrincipal.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
 import './TempPrincipal.css';
+import { Link } from 'react-router-dom';
 
-// import LoginTemplate from './LoginTemplate';
-// import RecoverPassTemplate from './RecoverPassTemplate';
+import LoginTemplate from './LoginTemplate';
 
 import RegistrationTemplate from './RegistrationTemplate';
 
 
 
 export default class TemplatePrincipal extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { active: false }
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-        event.preventDefault();
-        if (this.state.active === false)
-            this.setState({ active: true })
-    }
 
     render() {
         return (
             <React.Fragment>
-                {
-                    this.state.active ?
-                        <RegistrationTemplate />
-                        :
-                        <section className="principal">
-                            <div className="principal">
-                                <button onClick={this.handleClick} type="button" className="register">Cadastre-se</button>
-                                <button type="button" className="login">Login</button>
-                            </div>
-                        </section>
-                }
-
+                <section className="principal">
+                    <div className="principal">
+                        <button onClick={this.handleClickRegistration} type="button" className="register">Cadastre-se</button>
+                        <button type="button" className="login">Login</button>
+                    </div>
+                </section>
             </React.Fragment>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
